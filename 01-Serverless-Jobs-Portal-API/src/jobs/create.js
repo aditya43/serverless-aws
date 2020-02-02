e984c978f34641849a82a7b7f2e01955127542d5
Aditya Hajare <aditya43@gmail.com>
2020-02-02 19:32:53 +0530

    Create job lambda function

--- 01-Serverless-Jobs-Portal-API/src/jobs/create.js
+++ 01-Serverless-Jobs-Portal-API/src/jobs/create.js
@@ -1,19 +1,37 @@
-const jobs = [
-    {
-        id: 1,
-        title: 'ReactJS Developer'
-    },
-    {
-        id: 2,
-        title: 'NodeJS Developer'
-    }
-];
+const AWS = require('aws-sdk');
+const uuid = require('uuid');
+
+const dynamodb = new AWS.DynamoDB.DocumentClient({
+    region: 'localhost',
+    endpoint: 'http://localhost:8082'
+});
 
 module.exports.handler = async (event, context) => {
-    jobs.push(JSON.parse(event.body));
+    const job = JSON.parse(event.body);
+    const timeStamp = new Date().getTime();
 
-    return {
-        statusCode: 200,
-        body: JSON.stringify(jobs)
+    const params = {
+        TableName: 'jobs-portal',
+        Item: {
+            id: uuid.v1(),
+            title: job.title,
+            published: job.published,
+            createdAt: timeStamp,
+            updatedAt: timeStamp
+        }
     };
+
+    try {
+        await dynamodb.put(params).promise();
+
+        return {
+            statusCode: 201,
+            body: JSON.stringify(params.Item)
+        };
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify(error)
+        };
+    }
 };
