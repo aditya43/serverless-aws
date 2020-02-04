57e28c0ad7d669590663777b565523566a8d6114
Aditya Hajare <aditya43@gmail.com>
2020-02-04 18:54:58 +0530

    Scrape page function

--- 03-Yelp-Scraper/handler.js
+++ 03-Yelp-Scraper/handler.js
@@ -2,19 +2,15 @@
 
 const { getPage, parsePage, saveRatingsToDB } = require('./utils');
 
-module.exports.hello = async event => {
+module.exports.scrape = async event => {
+    getPage(event)
+        .then(page => console.log(page));
+
     return {
         statusCode: 200,
-        body: JSON.stringify(
-            {
-                message: 'Go Serverless v1.0! Your function executed successfully!',
-                input: event
-            },
-            null,
-            2
-        )
+        body: JSON.stringify({
+            message: 'Go Serverless v1.0! Your function executed successfully!',
+            input: event
+        }, null, 2)
     };
-
-    // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-    // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
--- 03-Yelp-Scraper/utils/getPage.js
+++ 03-Yelp-Scraper/utils/getPage.js
@@ -1,3 +1,7 @@
-module.exports = () => {
+const request = require('request-promise');
 
+module.exports = async businessName => {
+    const url = `https://www.yelp.com/biz/${businessName}`;
+
+    return request({ url, method: 'GET' });
 };
