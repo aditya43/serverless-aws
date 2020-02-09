332e3b877e41111ff5eb77832118b10c0401df92
Aditya Hajare <aditya43@gmail.com>
2020-02-09 19:48:09 +0530

    Sequelize todo model

---
+++ 04-REST-API/models/todo.js
@@ -0,0 +1,37 @@
+/**
+ * @param sequelize Database connection
+ * @param DataTypes Sequelize instance
+ */
+module.exports = (sequelize, DataTypes) => {
+    return sequelize.define('todo', {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        task: {
+            type: DataTypes.STRING
+        },
+        completed: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false
+        },
+        created_at: {
+            type: DataTypes.DATE,
+            defaultValue: sequelize.fn('NOW'),
+            allowNull: false
+        },
+        updated_at: {
+            type: DataTypes.DATE,
+            defaultValue: sequelize.fn('NOW'),
+            allowNull: false
+        },
+        deleted_at: {
+            type: DataTypes.DATE
+        }
+    }, {
+        underscored: true, // Sequelize by default expects column names to be camel case. Since we are using postgresql, column names cannot be camel case. Instead they are snake case.
+        paranoid: true // When anything is deleted from this table, it won't delete a row. Instead it will set 'deleted_at' key.
+    });
+};
