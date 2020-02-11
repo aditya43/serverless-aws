a6918b56a7049e03c2251a840951acc8618d9037
Aditya Hajare <aditya43@gmail.com>
2020-02-11 17:11:21 +0530

    List todos

--- 04-REST-API/features/read.js
+++ 04-REST-API/features/read.js
@@ -1,9 +1,19 @@
-'use strict';
+const db = require('../db');
 
-module.exports.getTodo = async event => {
-    const todo = 'Make dinner';
-    return {
-        statusCode: 200,
-        body: JSON.stringify({ todo }, null, 2)
-    };
+module.exports.listTodos = async event => {
+    try {
+        const tasks = await db.todo.findAll({
+            attributes: ['id', 'task', 'completed']
+        });
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ tasks }, null, 2)
+        };
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error }, null, 2)
+        };
+    }
 };
