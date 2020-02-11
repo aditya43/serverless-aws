023cc1a9818c6b0c66aa805889eb56285a61b3f9
Aditya Hajare <aditya43@gmail.com>
2020-02-11 17:45:09 +0530

    Delete single task

---
+++ 04-REST-API/features/delete.js
@@ -0,0 +1,37 @@
+const db = require('../db');
+
+module.exports.deleteTodo = async event => {
+    try {
+        const id = event.pathParameters.id;
+
+        if (id) {
+            const res = await db.todo.destroy({ where: { id } });
+
+            if (res) {
+                return {
+                    statusCode: 200,
+                    body: JSON.stringify({ message: 'Task has been deleted' }, null, 2)
+                };
+            }
+
+            return {
+                statusCode: 404,
+                body: JSON.stringify({ message: 'Task not found' }, null, 2)
+            };
+        }
+
+        return {
+            statusCode: 400,
+            body: JSON.stringify({ message: 'Missing task id' }, null, 2)
+        };
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({
+                message: error.message || '',
+                error: error
+            }, null, 2)
+        };
+    }
+}
+;
