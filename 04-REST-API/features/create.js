838f18ac6d2c6d48e8833bcd3571e8e891d1f316
Aditya Hajare <aditya43@gmail.com>
2020-02-11 15:17:22 +0530

    Create task | Added error handling

--- 04-REST-API/features/create.js
+++ 04-REST-API/features/create.js
@@ -1,19 +1,25 @@
 const db = require('../db');
 
 module.exports.createTodo = async event => {
-    const body = JSON.parse(event.body);
-    console.log(body);
-    if (!body.task) {
+    try {
+        const body = JSON.parse(event.body);
+
+        if (!body.task) {
+            return {
+                statusCode: 401,
+                body: JSON.stringify({ message: 'Invalid task' }, null, 2)
+            };
+        }
+
+        const task = await db.todo.create({ task: body.task });
         return {
-            statusCode: 401,
-            body: JSON.stringify({ message: 'Invalid task' }, null, 2)
+            statusCode: 201,
+            body: JSON.stringify({ todo: task }, null, 2)
+        };
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: db }, null, 2)
         };
     }
-
-    const task = await db.todo.create({ task: body.task });
-
-    return {
-        statusCode: 201,
-        body: JSON.stringify({ todo: task }, null, 2)
-    };
 };
