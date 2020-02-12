3bcf6448209e613a3e12e6a00e876f41470e3b75
Aditya Hajare <aditya43@gmail.com>
2020-02-12 19:12:13 +0530

    Moment timezone WIP

--- 05-S3-Notifications/mailer/generateContent.js
+++ 05-S3-Notifications/mailer/generateContent.js
@@ -1,7 +1,9 @@
+const momentTimeZone = require('moment-timezone');
+
 module.exports = () => {
     return new Promise((resolve, reject) => {
         try {
-
+            const timestamp = `${momentTimeZone.tz()}`;
         } catch (error) {
             console.log(error);
             reject(new Error(JSON.stringify(error)));
