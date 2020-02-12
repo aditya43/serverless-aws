c4c879b27f1fa449fb4ec32e64048a3e4c9cc09f
Aditya Hajare <aditya43@gmail.com>
2020-02-12 18:35:22 +0530

    Added moment timezone and nodemailer npm packages

--- 05-S3-Notifications/mailer/generateContent.js
+++ 05-S3-Notifications/mailer/generateContent.js
@@ -1,3 +1,10 @@
 module.exports = () => {
+    return new Promise((resolve, reject) => {
+        try {
 
+        } catch (error) {
+            console.log(error);
+            reject(new Error(JSON.stringify(error)));
+        }
+    });
 };
