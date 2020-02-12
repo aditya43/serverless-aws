2ceb1e20ec416854f24a11843595b653dd5ecc33
Aditya Hajare <aditya43@gmail.com>
2020-02-12 16:58:02 +0530

    S3 upload file event notification

--- 05-S3-Notifications/handler.js
+++ 05-S3-Notifications/handler.js
@@ -1,14 +1,19 @@
 module.exports.s3_notification = async event => {
-    console.log(event.Records[0].s3);
-    return {
-        statusCode: 200,
-        body: JSON.stringify(
-            {
-                message: 'Go Serverless v1.0! Your function executed successfully!',
-                input: event
-            },
-            null,
-            2
-        )
-    };
+    const uploadData = event.Records.map(record => ({
+        bucketName: record.s3.bucket.name,
+        file: record.s3.object.key,
+        size: record.s3.object.size
+    }))[0];
+    console.log(uploadData);
+    // return {
+    //     statusCode: 200,
+    //     body: JSON.stringify(
+    //         {
+    //             message: 'Go Serverless v1.0! Your function executed successfully!',
+    //             input: event
+    //         },
+    //         null,
+    //         2
+    //     )
+    // };
 };
