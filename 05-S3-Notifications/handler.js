745a1c5bce217ccae4ce6835d326fe148c96b3cd
Aditya Hajare <aditya43@gmail.com>
2020-02-13 14:58:09 +0530

    S3 Notifications mailer lambda function

--- 05-S3-Notifications/handler.js
+++ 05-S3-Notifications/handler.js
@@ -1,19 +1,39 @@
-module.exports.s3_notification = async event => {
-    const uploadData = event.Records.map(record => ({
-        bucketName: record.s3.bucket.name,
-        file: record.s3.object.key,
-        size: record.s3.object.size
-    }))[0];
-    console.log(uploadData);
-    // return {
-    //     statusCode: 200,
-    //     body: JSON.stringify(
-    //         {
-    //             message: 'Go Serverless v1.0! Your function executed successfully!',
-    //             input: event
-    //         },
-    //         null,
-    //         2
-    //     )
-    // };
+'use strict';
+const mailer = require('./mailer');
+
+module.exports.s3_notification = (event, context, callback) => {
+    // extract S3 data from the event object
+    const uploadData = event.Records.map(record => {
+        return {
+            bucketName: record.s3.bucket.name,
+            file: record.s3.object.key,
+            fileSize: record.s3.object.size
+        };
+    })[0];
+
+    // generate an email using the data above
+    mailer.generateContent(uploadData)
+        .then(content => mailer.sendEmail(content)) // send the email
+        .then(message => {
+            // send a response
+
+            const response = {
+                statusCode: 200,
+                body: JSON.stringify({
+                    message: message
+                })
+            };
+
+            callback(null, response);
+        })
+        .catch(error => {
+            const response = {
+                statusCode: 500,
+                body: JSON.stringify({
+                    error: error
+                })
+            };
+
+            callback(null, response);
+        });
 };
