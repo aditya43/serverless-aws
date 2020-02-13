eea2f2ba12db7350e94412733cd7abb97d535122
Aditya Hajare <aditya43@gmail.com>
2020-02-13 14:54:35 +0530

    Generate mail content

--- 05-S3-Notifications/mailer/generateContent.js
+++ 05-S3-Notifications/mailer/generateContent.js
@@ -1,11 +1,38 @@
 const momentTimeZone = require('moment-timezone');
 
-module.exports = () => {
+module.exports = data => {
     return new Promise((resolve, reject) => {
         try {
-            const timestamp = `${momentTimeZone.tz()}`;
+            // timestamp
+            const timestamp = `${momentTimeZone.tz('America/Los_Angeles').format('MMMM Do, h:mm:ss a')} PT`;
+            // subject line
+            const subject = `New upload to S3 Bucket: ${data.bucketName}`;
+            // text body
+            const textBody = `
+                Great news!
+                Someone has uploaded ${data.file} (${data.fileSize} bytes) to your AWS S3 bucket "${data.bucketName}" on ${timestamp}.
+                Congrats!
+                Sincerely,
+                Your Serverless Function`;
+
+            // html body
+            const htmlBody = `
+                <div style="max-width: 600px; margin: 20px auto">
+                <h1>Great news!</h1>
+                <p style="line-height: 22px; font-size: 16px;">Someone uploaded <b>${data.file}</b> (${data.fileSize} bytes) to your AWS S3 bucket "${data.bucketName}" on ${timestamp}.</p>
+                <p style="line-height: 22px; font-size: 16px;">Congrats!</p>
+                <p style="line-height: 22px; font-size: 16px;">Sincerely,</p>
+                <p style="line-height: 22px; font-size: 16px;"><i>Your Serverless Function</i></p>
+                </div>
+            `;
+            console.log('Generated content.');
+            resolve({
+                subject,
+                textBody,
+                htmlBody
+            });
         } catch (error) {
-            console.log(error);
+            console.error('Error generating email content.', error);
             reject(new Error(JSON.stringify(error)));
         }
     });
