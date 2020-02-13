70e2e87d1bc252743be5ddbdddedd0732ef085ec
Aditya Hajare <aditya43@gmail.com>
2020-02-13 14:55:50 +0530

    Sendmail

--- 05-S3-Notifications/mailer/sendEmail.js
+++ 05-S3-Notifications/mailer/sendEmail.js
@@ -1,3 +1,41 @@
-module.exports = () => {
+const mailer = require('nodemailer');
+const config = require('./config');
 
+module.exports = content => {
+    return new Promise((resolve, reject) => {
+        // configure transporter
+        const transporter = mailer.createTransport({
+            host: 'smtp.gmail.com',
+            port: 587,
+            secure: false,
+            service: 'gmail',
+            auth: {
+                user: config.user,
+                pass: config.password
+            }
+        });
+        // create recipient string
+        let recipients = '';
+        config.to.forEach(recipient => (recipients += `${recipient}, `));
+        // define email options
+        const mailOptions = {
+            from: 'person@gmail.com',
+            to: recipients,
+            subject: content.subject,
+            text: content.textBody,
+            html: content.htmlBody
+        };
+        // send the email
+        return transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                const errorMessage = JSON.stringify(error);
+                console.error('Error sending email.', errorMessage);
+                reject(new Error(errorMessage));
+            } else {
+                const successMessage = `Message ${info.messageId} send: ${info.response}`;
+                console.log(successMessage);
+                resolve(successMessage);
+            }
+        });
+    });
 };
