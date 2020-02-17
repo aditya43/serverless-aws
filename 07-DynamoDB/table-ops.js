3c138f023cd90e7d79946c11abcad71d189a31cd
Aditya Hajare <aditya43@gmail.com>
2020-02-17 07:46:14 +0530

    DynamoDB list tables

--- 07-DynamoDB/table-ops.js
+++ 07-DynamoDB/table-ops.js
@@ -3,3 +3,11 @@ const AWS = require('aws-sdk');
 AWS.config.update({ region: 'ap-south-1' });
 
 const dynamoDB = new AWS.DynamoDB();
+
+dynamoDB.listTables({}, (err, data) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(data);
+    }
+});
