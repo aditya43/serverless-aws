121c98d0300f8bd6a7ec241c6e4c01ac36b1acc4
Aditya Hajare <aditya43@gmail.com>
2020-02-18 12:36:15 +0530

    DynamoDB delete item

---
+++ 07-DynamoDB/tables-ops/Items-Level-Ops/03-deleteItem.js
@@ -0,0 +1,19 @@
+const AWS = require('aws-sdk');
+
+AWS.config.update({ region: 'ap-south-1' });
+
+const docClient = new AWS.DynamoDB.DocumentClient();
+
+docClient.delete({
+    TableName: 'adi_notes_app',
+    Key: {
+        user_id: 'test123',
+        timestamp: 1
+    }
+}, (err, data) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(JSON.stringify(data, null, 2));
+    }
+});
