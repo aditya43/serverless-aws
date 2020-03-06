60ac1d298931c71d62d647b1c0ebaf8cff04d332
Aditya Hajare <aditya43@gmail.com>
2020-03-06 21:16:58 +0530

    Image Processor State Machine | Lambda Function to write resized image metadata into DynamoDB table | Done

--- 10-Image-Processor-Using-Step-Functions/06-writeToDynamoDB-Lambda-Function.js
+++ 10-Image-Processor-Using-Step-Functions/06-writeToDynamoDB-Lambda-Function.js
@@ -9,6 +9,46 @@ AWS.config.update({ region: 'ap-south-1' });
 
 const documentClient = new AWS.DynamoDB.DocumentClient();
 
+const processImageMeta = async images => {
+    let original, thumbnail;
+
+    const filesProcessed = images.map(async image => {
+        for (const key in image) {
+            switch (key) {
+                case 'original':
+                    original = `${image.original.region}|${image.original.bucket}|${image.original.key}`;
+                    break;
+
+                case 'resized': // Thumbnail
+                    thumbnail = `${image.thumbnail.region}|${image.thumbnail.bucket}|${image.thumbnail.key}`;
+                    break;
+
+                default:
+                    break;
+            }
+        }
+    });
+
+    await Promise.all(filesProcessed);
+
+    return {
+        original,
+        thumbnail
+    };
+};
+
 exports.handler = async event => {
     const images = await processImageMeta(event.results.images);
+
+    const params = {
+        TableName: process.env.RESIZED_IMAGES_METADATA_TABLENAME, // Add DynamoDB table name to store resized images metadata to environment variable.
+        Item: {
+            original: images.original,
+            thumbnail: images.thumbnail,
+            timestamp: new Date().getTime()
+        }
+    };
+
+    await documentClient.put(params).promise();
+    return true;
 };
