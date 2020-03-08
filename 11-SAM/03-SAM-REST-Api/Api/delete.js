0b045b68769473e2ac27e6bd0e70f5f7d7382f94
Aditya Hajare <aditya43@gmail.com>
2020-03-08 11:39:09 +0530

    AWS SAM | REST API | REST API | DELETE lambda function

--- 11-SAM/03-SAM-REST-Api/Api/delete.js
+++ 11-SAM/03-SAM-REST-Api/Api/delete.js
@@ -7,10 +7,17 @@ const documentClient = new AWS.DynamoDB.DocumentClient();
 const tableName = process.env.TABLE_NAME;
 
 exports.handler = async event => {
+    const userid = event.pathParameters.userid;
+
+    await documentClient.delete({
+        TableName: tableName,
+        Key: { userid }
+    }).promise();
+
     return {
         statusCode: 200,
         body: JSON.stringify({
-
+            message: 'User deleted successfully'
         })
     };
 };
