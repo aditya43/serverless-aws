d7ef0003f416808395896cf5cb617f44152b5ecf
Aditya Hajare <aditya43@gmail.com>
2020-03-08 11:36:26 +0530

    AWS SAM | REST API | REST API | GET lambda function

--- 11-SAM/03-SAM-REST-Api/Api/get.js
+++ 11-SAM/03-SAM-REST-Api/Api/get.js
@@ -7,10 +7,21 @@ const documentClient = new AWS.DynamoDB.DocumentClient();
 const tableName = process.env.TABLE_NAME;
 
 exports.handler = async event => {
-    return {
-        statusCode: 200,
-        body: JSON.stringify({
+    const userid = event.pathParameters.userid;
 
-        })
-    };
+    const data = await documentClient.get({
+        TableName: tableName,
+        Key: {
+            userid
+        }
+    }).promise();
+
+    if (data.Item) {
+        return {
+            statusCode: 200,
+            body: JSON.stringify(data.Item)
+        };
+    } else {
+        throw new Error('User not found');
+    }
 };
