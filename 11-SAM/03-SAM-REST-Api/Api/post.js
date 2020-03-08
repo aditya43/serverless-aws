fb2d4066d3de47162daf16a907fc1d9f8458c28f
Aditya Hajare <aditya43@gmail.com>
2020-03-08 11:33:31 +0530

    AWS SAM | REST API | REST API | POST lambda function

--- 11-SAM/03-SAM-REST-Api/Api/post.js
+++ 11-SAM/03-SAM-REST-Api/Api/post.js
@@ -7,10 +7,27 @@ const documentClient = new AWS.DynamoDB.DocumentClient();
 const tableName = process.env.TABLE_NAME;
 
 exports.handler = async event => {
+    const userid = event.pathParameters.userid;
+    const { firstName, lastName, email, website } = JSON.parse(event.body);
+
+    const item = {
+        userid,
+        firstName,
+        lastName,
+        email,
+        website
+    };
+
+    const data = await documentClient.put({
+        TableName: tableName,
+        Item: item
+    }).promise();
+
     return {
         statusCode: 200,
         body: JSON.stringify({
-
+            message: 'Data inserted/updated successfully',
+            data: data
         })
     };
 };
