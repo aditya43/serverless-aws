0db704433ed27798cdcf6ac612540d71c5dd629a
Aditya Hajare <aditya43@gmail.com>
2020-03-11 20:12:31 +0530

    Serverless Notes App | Get Notes - Paginated Results - Lambda Function

--- 14-Serverless-Notes-App/sls-notes-app-backened/api/get-notes.js
+++ 14-Serverless-Notes-App/sls-notes-app-backened/api/get-notes.js
@@ -8,14 +8,37 @@ const helpers = require('../utils/helpers');
 AWS.config.update({ region: 'ap-south-1' });
 
 const dynamoDB = new AWS.DynamoDB.DocumentClient();
-const tableName = process.env.NOTES_TABLE;
+const TableName = process.env.NOTES_TABLE;
 
 exports.handler = async event => {
     try {
+        const limit = event.queryStringParameters.limit || 5;
+        const startTimeStamp = event.queryStringParameters.start || 0;
+        const userId = helpers.getUserId(event.headers);
+
+        const params = {
+            TableName,
+            KeyConditionExpression: 'user_id = :uid',
+            ExpressionAttributeValues: {
+                ':uid': userId
+            },
+            Limit: parseInt(limit),
+            ScanIndexForward: false // This will return data in descending order of sort key. The sort key in our case is timestamp.
+        };
+
+        if (parseInt(startTimeStamp) > 0) {
+            params.ExclusiveStartKey = {
+                user_id: userId,
+                timestamp: startTimeStamp
+            };
+        }
+
+        const data = await dynamoDB.query(params).promise();
+
         return {
             statusCode: 200,
             headers: helpers.getResponseHeaders(),
-            body: JSON.stringify('')
+            body: JSON.stringify(data, null, 2)
         };
     } catch (err) {
         console.log(err);
