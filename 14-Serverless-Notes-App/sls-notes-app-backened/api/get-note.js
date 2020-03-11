9bb85b2a5729c6c47cc85218822b1932b7e770be
Aditya Hajare <aditya43@gmail.com>
2020-03-11 20:20:15 +0530

    Serverless Notes App | Get Note Lambda Function

--- 14-Serverless-Notes-App/sls-notes-app-backened/api/get-note.js
+++ 14-Serverless-Notes-App/sls-notes-app-backened/api/get-note.js
@@ -3,19 +3,41 @@
  */
 
 const AWS = require('aws-sdk');
+const _ = require('underscore');
 const helpers = require('../utils/helpers');
 
 AWS.config.update({ region: 'ap-south-1' });
 
 const dynamoDB = new AWS.DynamoDB.DocumentClient();
-const tableName = process.env.NOTES_TABLE;
+const TableName = process.env.NOTES_TABLE;
 
 exports.handler = async event => {
     try {
+        const noteId = decodeURIComponent(event.pathParameters.note_id);
+
+        const params = {
+            TableName,
+            IndexName: 'note_id-index',
+            KeyConditionExpression: 'note_id = :note_id',
+            ExpressionAttributeValues: {
+                ':note_id': noteId
+            },
+            Limit: 1
+        };
+
+        const data = await dynamoDB.query(params).promise();
+
+        if (!_.isEmpty(data.Items)) {
+            return {
+                statusCode: 200,
+                headers: helpers.getResponseHeaders(),
+                body: JSON.stringify(data.Items[0], null, 2)
+            };
+        }
+
         return {
-            statusCode: 200,
-            headers: helpers.getResponseHeaders(),
-            body: JSON.stringify('')
+            statusCode: 404,
+            headers: helpers.getResponseHeaders()
         };
     } catch (err) {
         console.log(err);
