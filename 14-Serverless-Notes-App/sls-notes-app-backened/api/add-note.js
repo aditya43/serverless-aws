446e5c8d5fe5c29746a93433c41c1303e873cf9f
Aditya Hajare <aditya43@gmail.com>
2020-03-11 19:54:02 +0530

    Serverless Notes App | Add Note Lambda Function

--- 14-Serverless-Notes-App/sls-notes-app-backened/api/add-note.js
+++ 14-Serverless-Notes-App/sls-notes-app-backened/api/add-note.js
@@ -3,19 +3,33 @@
  */
 
 const AWS = require('aws-sdk');
+
+const moment = require('moment');
+const uuidv5 = require('uuid/v5');
 const helpers = require('../utils/helpers');
 
 AWS.config.update({ region: 'ap-south-1' });
 
 const dynamoDB = new AWS.DynamoDB.DocumentClient();
-const tableName = process.env.NOTES_TABLE;
+const TableName = process.env.NOTES_TABLE;
 
 exports.handler = async event => {
     try {
+        const Item = JSON.parse(even.body).Item;
+
+        Item.user_id = helpers.getUserId(event.headers);
+        Item.user_name = helpers.getUserName(event.headers);
+        Item.note_id = `${Item.user_id}:${uuidv5()}`;
+        Item.timestamp = moment().unix();
+        Item.expires = moment().add(90, 'days')
+            .unix();
+
+        await dynamoDB.put({ TableName, Item }).promise();
+
         return {
             statusCode: 200,
             headers: helpers.getResponseHeaders(),
-            body: JSON.stringify('')
+            body: JSON.stringify(Item, null, 2)
         };
     } catch (err) {
         console.log(err);
--- 14-Serverless-Notes-App/sls-notes-app-backened/utils/helpers.js
+++ 14-Serverless-Notes-App/sls-notes-app-backened/utils/helpers.js
@@ -3,3 +3,11 @@ exports.getResponseHeaders = () => {
         'Access-Control-Allow-Origin': '*'
     };
 };
+
+exports.getUserId = headers => {
+    return headers.app_user_id;
+};
+
+exports.getUserName = headers => {
+    return headers.app_user_name;
+};
